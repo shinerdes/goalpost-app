5c6fb8dc8ec340cd8b5890d02bbc7fbd145110c1
김영석 <yeongseok@gim-yeongseog-ui-MacBook-Pro.local>
2018-10-04 18:51:41 +0900

    building goalcell

---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by 김영석 on 2018. 10. 4..
+//  Copyright © 2018년 김영석. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int)
+    {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+}
