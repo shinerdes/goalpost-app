a3f3598c628d928f7b3813a916107488e862010b
김영석 <yeongseok@gim-yeongseog-ui-MacBook-Pro.local>
2018-10-04 19:45:00 +0900

    Displaying Static GoalCells in UITableView / Creating GoalType Enum

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -16,7 +16,9 @@ class GoalsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
     }
 
    
@@ -26,6 +28,24 @@ class GoalsVC: UIViewController {
     }
     
     
+    
+    
 
+}
+
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
+        cell.configureCell(description: "Eat saled twice a week.", type: .shortTerm, goalProgressAmount: 2)
+        return cell
+    }
 }
 
