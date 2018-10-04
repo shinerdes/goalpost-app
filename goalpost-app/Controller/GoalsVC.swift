037314e79c6fc6901b0692a71f180f884f549760
김영석 <yeongseok@gim-yeongseog-ui-MacBook-Pro.local>
2018-10-04 19:23:49 +0900

    core data

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class GoalsVC: UIViewController {
 
@@ -15,13 +16,10 @@ class GoalsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
         
         print("button was pressed")
