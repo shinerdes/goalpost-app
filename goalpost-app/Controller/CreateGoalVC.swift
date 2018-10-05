9e318e0a47636050a29fe9227cf9405910322107
김영석 <yeongseok@gim-yeongseog-ui-MacBook-Pro.local>
2018-10-05 17:30:16 +0900

    Fixing Dismissal of FinishGoalVC

--- goalpost-app/Controller/CreateGoalVC.swift
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -45,7 +45,7 @@ class CreateGoalVC: UIViewController, UITextViewDelegate {
         if goalTextView.text != "" && goalTextView.text != "What is your goal?" {
             guard let finishGoalVC = storyboard?.instantiateViewController(withIdentifier: "finishGoalVC") as? FinishGoalVC else { return }
             finishGoalVC.initData(description: goalTextView.text!, type: goalType)
-            presentDetail(viewControllerToPresent: finishGoalVC)
+            presentingViewController?.presentSecondaryDetail(finishGoalVC)
         }
     }
    
